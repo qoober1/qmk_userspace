a167818cb849338957b90f4c10da927137b1b060
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2024-01-08 21:53:21 -0700

    settled on an aptv5 variant, reshuffled some layer edges

--- users/possumvibes/archive/alt-layouts.h
+++ users/possumvibes/archive/alt-layouts.h
@@ -1,61 +1,30 @@
 /* alt layouts: not in use */
 // APTHD. APTv3 variant with reduced load on pinkies. Not in use--put too much load on index for me
-#define __________________APTHD_L1_________________       KC_W,    KC_C,    KH_Y,    KC_D,    KC_B
-#define __________________APTHD_L2_________________       KH_R,    KH_S,    KH_T,    KH_H,    KC_K
-#define __________________APTHD_L3_________________       KC_X,    KC_F,    KC_G,    KC_P,    KC_V
+#define LAYER_APTHD \
+    KC_W,    KC_C,    KH_Y,    KC_D,    KC_B,    KC_Q,    KC_L,    KH_U,    KC_O,    KC_QUOT, \
+    KH_R,    KH_S,    KH_T,    KH_H,    KC_K,    KC_J,    KH_N,    KH_E,    KH_A,    KH_I,    \
+    KC_X,    KC_F,    KC_G,    KC_P,    KC_V,    KC_Z,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
+                               THM_LH0, THM_LH1, THM_RH1, THM_RH0
 
-#define __________________APTHD_R1_________________       KC_Q,    KC_L,    KH_U,    KC_O,    KC_QUOT
-#define __________________APTHD_R2_________________       KC_J,    KH_N,    KH_E,    KH_A,    KH_I
-#define __________________APTHD_R3_________________       KC_Z,    KC_M,    KC_COMM, KC_DOT, KC_SLSH
+// AptMak
+#define LAYER_APTMAK \
+    KC_Q,    KC_W,    KH_F,    KC_P,    KC_B,    KC_Z,    KC_L,    KH_U,    KC_Y,    KC_QUOT, \
+    KH_R,    KH_S,    KH_T,    KH_H,    KC_K,    KC_J,    KH_N,    KM_A,    KS_I,    KC_O,    \
+    KC_X,    KC_C,    KC_G,    KC_D,    KC_V,    XXXXXXX, KC_M,    KC_COMM, KC_DOT,  XXXXXXX, \
+                               KC_SPC,  NAVMOD,  OS_LSFT, KC_EE
+
+// APTv5: my first 30 key layout, intended for v on combo. Turns out I don't care for that,
+// so V is back on pinky for the moment so it's available when using this layout.
+#define LAYER_APTv5 \
+    KC_W,    KC_C,    KC_G,    KC_D,    KC_QUOT, KC_X,    KC_L,    KC_U,    KC_O,    KC_Y,    \
+    KC_R,    KC_S,    KC_T,    KC_H,    KC_K,    KC_J,    KC_N,    KC_E,    KC_A,    KC_I,    \
+    KC_V,    KC_B,    KC_P,    KC_F,    KC_Q,    KC_Z,    KC_M,    KC_COMM, KC_DOT,  XXXXXXX, \
+                               THM_LH0, THM_LH1, THM_RH1, THM_RH0
+
 
 // Canary
-#define _________________CANARY_L1_________________       KC_W,    KC_L,    KH_Y,    KC_P,    KC_B
-#define _________________CANARY_L2_________________       KS_C,    KS_R,    KS_S,    KS_T,    KC_G
-#define _________________CANARY_L3_________________       KC_Q,    KC_J,    KC_V,    KC_D,    KC_K
-
-#define _________________CANARY_R1_________________       KC_Z,    KC_F,    KS_O,    KC_U,    KC_QUOT
-#define _________________CANARY_R2_________________       KC_M,    KH_N,    KH_E,    KS_I,    KY_A
-#define _________________CANARY_R3_________________       KC_X,    KC_H,    KC_SCLN,  KC_COMM, KC_DOT
-
-// APT 3 Meets HD, not presently in use
-#define ________________APTv3_HD_L1________________       KC_W,    KC_G,    KH_D,    KC_F,    KC_B
-#define ________________APTv3_HD_L2________________       KH_R,    KH_S,    KH_T,    KH_H,    KC_K
-#define ________________APTv3_HD_L3________________       KC_X,    KC_C,    KC_Y,    KC_P,    KC_V
-
-#define ________________APTv3_HD_R1________________       KC_Q,    KC_L,    KH_U,    KC_O,    KC_QUOT
-#define ________________APTv3_HD_R2________________       KC_J,    KH_N,    KH_E,    KH_A,    KH_I
-#define ________________APTv3_HD_R3________________       KC_Z,    KC_M,    KC_COMM, KC_DOT, XXXXXXX
-
-// AptMak 
-#define ________________APTMAK_L1__________________       KC_Q,    KC_W,    KH_F,    KC_P,    KC_B
-#define ________________APTMAK_L2__________________       KH_R,    KH_S,    KH_T,    KH_H,    KC_K
-#define ________________APTMAK_L3__________________       KC_X,    KC_C,    KC_G,    KC_D,    KC_V
-#define _APTMAK_THUMB_L_                                  KC_SPC,  NAVMODE
-
-#define ________________APTMAK_R1__________________       KC_Z,    KC_L,    KH_U,    KC_Y,    KC_QUOT
-#define ________________APTMAK_R2__________________       KC_J,    KH_N,    KM_A,    KS_I,    KC_O
-#define ________________APTMAK_R3__________________       XXXXXXX, KC_M,    KC_COMM, KC_DOT,  XXXXXXX
-#define _APTMAK_THUMB_R_                                  OS_LSFT, KC_E
-
-// APTHD2!
-#define ________________APTHD2_L1__________________       XXXXXXX, KC_F,    KH_G,    KC_D,    KC_B
-#define ________________APTHD2_L2__________________       KH_R,    KH_S,    KH_T,    KH_H,    KC_K
-#define ________________APTHD2_L3__________________       KC_X,    KC_C,    KC_Y,    KC_P,    KC_V
-#define _APTHD2_THUMB_L_                                  NAVMODE, OSR_SFT
-
-#define ________________APTHD2_R1__________________       KC_J,    KC_L,    KH_U,    KC_O,    XXXXXXX 
-#define ________________APTHD2_R2__________________       KC_W,    KH_N,    KH_E,    KH_A,    KH_I
-#define ________________APTHD2_R3__________________       XXXXXXX, KC_M,    KC_COMM, KC_DOT,  XXXXXXX
-#define _APTHD2_THUMB_R_                                  KC_QUOT, KC_SPC
-
-// HDLIKE apt variant
-#define ________________HDLIKE_L1__________________       KC_X,    KC_C,    KH_D,    KC_L,    KC_K
-#define ________________HDLIKE_L2__________________       KH_R,    KH_S,    KH_T,    KH_H,    KC_J
-#define ________________HDLIKE_L3__________________       KC_X,    KC_G,    KC_B,    KC_M,    KC_V
-#define _HDLIKE_THUMB_L_                                  NAVMODE, OSR_SFT
-
-#define ________________HDLIKE_R1__________________       KC_W,    KC_Y,    KH_U,    KC_O,    XXXXXXX 
-#define ________________HDLIKE_R2__________________       KC_P,    KH_N,    KH_E,    KH_A,    KH_I
-#define ________________HDLIKE_R3__________________       XXXXXXX, KC_F,    KC_COMM, KC_DOT,  XXXXXXX
-#define _HDLIKE_THUMB_R_                                  KC_QUOT, KC_SPC
-
+#define LAYER_CANARY \
+    KC_W,    KC_L,    KH_Y,    KC_P,    KC_B,    KC_Z,    KC_F,    KS_O,    KC_U,    KC_QUOT, \
+    KS_C,    KS_R,    KS_S,    KS_T,    KC_G,    KC_M,    KH_N,    KH_E,    KS_I,    KY_A,    \
+    KC_Q,    KC_J,    KC_V,    KC_D,    KC_K,    KC_X,    KC_H,    KC_SCLN,  KC_COMM, KC_DOT, \
+                               THM_LH0, THM_LH1, THM_RH1, THM_RH0
